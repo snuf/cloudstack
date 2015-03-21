972349a79881dbeb95f5ccaca1d2c9f3ee4cc2d0
Funs <funs@barred.org>
2015-03-21 10:43:14 +0100

    Openvswitch support, NVP support and Primary Storage GUI bug fixed.
    Openvswitch tested with 3.2/3.3 with openvswitch 2.3.1
    NVP not tested yet.
    Primary Storage filesystem selection was broken for OVM3 in the GUI

--- plugins/hypervisors/ovm3/src/test/java/com/cloud/hypervisor/ovm3/objects/NetworkTest.java
+++ plugins/hypervisors/ovm3/src/test/java/com/cloud/hypervisor/ovm3/objects/NetworkTest.java
@@ -21,6 +21,8 @@ import javax.naming.ConfigurationException;
 import org.junit.Test;
 import com.cloud.hypervisor.ovm3.support.Ovm3SupportTest;
 
+import com.cloud.hypervisor.ovm3.support.Ovm3SupportTest;
+
 public class NetworkTest {
     ConnectionTest con = new ConnectionTest();
     Network net = new Network(con);
